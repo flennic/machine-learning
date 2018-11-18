8d17d4b9e9ae921aff9cbc4a3cb1436a98638ad9
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-11-18 21:31:53 +0100

    Special Task 2 first implementation

--- lab01/special_taks.R
+++ lab01/special_taks.R
@@ -18,7 +18,7 @@ knearest = function(data, K = 5, newdata) {
     return(D)  
   }
   
-  D = d(data, newdata)
+  D = d(data[,-49], newdata[-49])
   
   classification = c()
   
@@ -29,8 +29,8 @@ knearest = function(data, K = 5, newdata) {
     classification = c(classification, (sum(newdata$Spam[indexesKnn]) > (K/2)))
   }
   
- classifiedCorrectly = sum(classification == newdata$Spam)
- missclassificationRate = 1 - (classifiedCorrectly/nrow(newdata))
+  classifiedCorrectly = sum(classification == newdata$Spam)
+  missclassificationRate = 1 - (classifiedCorrectly/nrow(newdata))
   
   return(missclassificationRate)
 }
@@ -38,7 +38,47 @@ knearest = function(data, K = 5, newdata) {
 c_data = spambase[1:(nrow(spambase)/2),]
 c_newdata = spambase[((nrow(spambase)/2)+1):nrow(spambase),]
 
-dm = knearest(c_data, K = 30, c_newdata)
+#dm = knearest(c_data, K = 30, c_data)
+#dm2 = knearest(c_data, K = 30, c_newdata)
 #dm2 = knearest(c_data, K = 1, c_data)
-print(dm)
+#print(dm)
 #print(dm2)
+
+
+density_estimation = function(data, K = 6, X) {
+  
+  N = length(data)
+  S = data
+  # V needs to be calculated
+  distances = abs(X - S)
+  sorted_distances = sort(distances, index.return = TRUE)
+  knearest = sorted_distances$x[1:K]
+  V = max(knearest) - min(knearest)
+  
+  return(K/(N*V))
+  
+  #D = X %*% t(X)
+  
+  #D = matrix(0, ncol = length(X), nrow = 0)
+  
+  #for (i in 1:length(X)) {
+  #  current_distances = c()
+  #  for (j in 1:length(X)) {
+  #    current_distances = cbind(current_distances, abs(X[i] - X[j]))
+  #  }
+  #  D = rbind(D, current_distances)
+  #}
+  
+  #dataSorted = sort(data$speed, index.return = TRUE)
+  
+  #densities = c()
+  
+  #for (i in 1:length(X)) {
+    
+  #}
+  
+  
+}
+
+a = density_estimation(cars$speed, K = 6, X = 3)
+print(a)
