e41a806ef955edbd402c606ba254d75b87d9a1f9
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-11-19 20:52:19 +0100

    First finalized version

--- lab01/special_taks.R
+++ lab01/special_taks.R
@@ -8,9 +8,11 @@ knearest = function(data, K = 5, newdata) {
   
   d = function(X, Y) {
     
+    # Make sure the input in the matrix format
     X = as.matrix(X)
     Y = as.matrix(Y)
     
+    # Calculate D as described in the exercise
     X_hat = X/sqrt(rowSums(X^2))
     Y_hat = Y/sqrt(rowSums(Y^2))
     C = X_hat %*% t(Y_hat)
@@ -19,32 +21,62 @@ knearest = function(data, K = 5, newdata) {
     return(D)  
   }
   
+  # Get D (trim classification column)
   D = d(data[,-49], newdata[-49])
   
+  classificationRate = c()
   classification = c()
+  classifiedCorrectly = c()
   
+  # For each data entry
   for (i in 1:nrow(D)) {
+    # Sort the distances and also get their index
     sortedRow = sort(D[,i], index.return = TRUE)
+    
+    # Take the K best guys and save their indexed
     indexesKnn = sortedRow$ix[1:K]
+    
+    # Lookup if they're classified as 0 or 1
     classificationRates = newdata$Spam[indexesKnn]
-    classification = c(classification, (sum(newdata$Spam[indexesKnn]) > (K/2)))
-  }
+    
+    # Add the classification
+    #classification = c(classification, (sum(newdata$Spam[indexesKnn]) > (K/2)))
+    classificationRate = c(classificationRate, (sum(newdata$Spam[indexesKnn] / K)))
+    temp_classification = sum(newdata$Spam[indexesKnn]) > (K/2)
+    classification = c(classification, temp_classification)
+    classifiedCorrectly = c(classifiedCorrectly, temp_classification != as.logical(data$Spam[i]))
+    }
   
-  classifiedCorrectly = sum(classification == newdata$Spam)
-  missclassificationRate = 1 - (classifiedCorrectly/nrow(newdata))
+  # Look how many got classified correctly
+  #classifiedCorrectly = sum(classification == newdata$Spam)
   
-  return(missclassificationRate)
+  # Take "1 -" to get the missclassification rate
+  #missclassificationRate = 1 - (classifiedCorrectly/nrow(newdata))
+  
+  returnDataFrame = cbind(newdata, classificationRate)
+  returnDataFrame = cbind(returnDataFrame, classification)
+  returnDataFrame = cbind(returnDataFrame, classifiedCorrectly)
+  
+  return(returnDataFrame)
 }
 
+spambase = read_excel("spambase.xlsx")
 c_data = spambase[1:(nrow(spambase)/2),]
 c_newdata = spambase[((nrow(spambase)/2)+1):nrow(spambase),]
 
-#dm = knearest(c_data, K = 30, c_data)
-#dm2 = knearest(c_data, K = 30, c_newdata)
-#dm2 = knearest(c_data, K = 1, c_data)
-#print(dm)
-#print(dm2)
+# Get the missqualification rates for training and  test
+training_classification = knearest(c_data, K = 30, c_data)
+test_classification = knearest(c_data, K = 30, c_newdata)
 
+# Get the classification rate
+training_rate = sum(training_classification$classification > 0.5) /
+  nrow(training_classification)
+test_rate = sum(test_classification$classification > 0.5) /
+  nrow(test_classification)
+
+# Print the results
+print(head(training_classification[,48:52]))
+print(head(test_classification[,48:52]))
 
 density_estimation = function(data, K = 6, X) {
   
@@ -64,15 +96,15 @@ min_speed = min(cars$speed)
 max_speed = max(cars$speed)
 steps = 1000
 
-x_values = seq(min_speed, max_speed, by = (max_speed - min_speed)/steps)
-y_values = unlist(lapply(x_values, function(x) density_estimation(cars$speed, K = 6, x)))
+#x_values = seq(min_speed, max_speed, by = (max_speed - min_speed)/steps)
+#y_values = unlist(lapply(x_values, function(x) density_estimation(cars$speed, K = 6, x)))
 
-density_data_frame = data.frame(x_values, y_values)
-colnames(density_data_frame) = c("X", "Density")
+#density_data_frame = data.frame(x_values, y_values)
+#colnames(density_data_frame) = c("X", "Density")
 
 #print(ggplot(density_data_frame, aes(x = X, y = Density)) + geom_line())
 
-print(ggplot(density_data_frame) +
-  geom_line(aes(x = X, y = Density, colour = "Density Function")) +
-  labs(title="Density of cars$speed", y="Density", x="X", color = "Legend") +
-  scale_color_manual(values = c("orange")))
+#print(ggplot(density_data_frame) +
+#  geom_line(aes(x = X, y = Density, colour = "Density Function")) +
+#  labs(title="Density of cars$speed", y="Density", x="X", color = "Legend") +
+#  scale_color_manual(values = c("orange")))
