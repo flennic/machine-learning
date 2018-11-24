57ac98cd9fbc201b2a152a7f6e5c202a9fd564ba
Maximilian Pfundstein <maximilian@pfundstein.org>
2018-11-24 12:28:12 +0100

    Take proper interval length for V

--- lab01/special_taks.R
+++ lab01/special_taks.R
@@ -60,23 +60,23 @@ knearest = function(data, K = 5, newdata) {
   return(returnDataFrame)
 }
 
-spambase = read_excel("spambase.xlsx")
-c_data = spambase[1:(nrow(spambase)/2),]
-c_newdata = spambase[((nrow(spambase)/2)+1):nrow(spambase),]
+#spambase = read_excel("spambase.xlsx")
+#c_data = spambase[1:(nrow(spambase)/2),]
+#c_newdata = spambase[((nrow(spambase)/2)+1):nrow(spambase),]
 
 # Get the missqualification rates for training and  test
-training_classification = knearest(c_data, K = 30, c_data)
-test_classification = knearest(c_data, K = 30, c_newdata)
+#training_classification = knearest(c_data, K = 30, c_data)
+#test_classification = knearest(c_data, K = 30, c_newdata)
 
 # Get the classification rate
-training_rate = sum(training_classification$classification > 0.5) /
-  nrow(training_classification)
-test_rate = sum(test_classification$classification > 0.5) /
-  nrow(test_classification)
+#training_rate = sum(training_classification$classification > 0.5) /
+#  nrow(training_classification)
+#test_rate = sum(test_classification$classification > 0.5) /
+#  nrow(test_classification)
 
 # Print the results
-print(head(training_classification[,48:52]))
-print(head(test_classification[,48:52]))
+#print(head(training_classification[,48:52]))
+#print(head(test_classification[,48:52]))
 
 density_estimation = function(data, K = 6, X) {
   
@@ -86,7 +86,8 @@ density_estimation = function(data, K = 6, X) {
   distances = abs(X - S)
   sorted_distances = sort(distances, index.return = TRUE)
   idx_knearest = sorted_distances$ix[1:K]
-  V = max(data[idx_knearest]) - min(data[idx_knearest])
+  #V = max(data[idx_knearest]) - min(data[idx_knearest])
+  V = 2 * abs(max(data[idx_knearest]) - X)
   #V = abs(data[idx_knearest[K]] - data[idx_knearest[1]])
   
   return(K/(N*V))
@@ -96,15 +97,15 @@ min_speed = min(cars$speed)
 max_speed = max(cars$speed)
 steps = 1000
 
-#x_values = seq(min_speed, max_speed, by = (max_speed - min_speed)/steps)
-#y_values = unlist(lapply(x_values, function(x) density_estimation(cars$speed, K = 6, x)))
+x_values = seq(min_speed, max_speed, by = (max_speed - min_speed)/steps)
+y_values = unlist(lapply(x_values, function(x) density_estimation(cars$speed, K = 6, x)))
 
-#density_data_frame = data.frame(x_values, y_values)
-#colnames(density_data_frame) = c("X", "Density")
+density_data_frame = data.frame(x_values, y_values)
+colnames(density_data_frame) = c("X", "Density")
 
-#print(ggplot(density_data_frame, aes(x = X, y = Density)) + geom_line())
+print(ggplot(density_data_frame, aes(x = X, y = Density)) + geom_line())
 
-#print(ggplot(density_data_frame) +
-#  geom_line(aes(x = X, y = Density, colour = "Density Function")) +
-#  labs(title="Density of cars$speed", y="Density", x="X", color = "Legend") +
-#  scale_color_manual(values = c("orange")))
+print(ggplot(density_data_frame) +
+  geom_line(aes(x = X, y = Density, colour = "Density Function")) +
+  labs(title="Density of cars$speed", y="Density", x="X", color = "Legend") +
+  scale_color_manual(values = c("orange")))
